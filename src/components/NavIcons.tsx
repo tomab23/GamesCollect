65c0c9b01780d7db67cf52045118498a6a068a98
thomas bartier <thomas.bartier@gmail.com>
2026-01-12 14:23:25 +0100

    navbar update

---
+++ src/components/NavIcons.tsx
@@ -0,0 +1,68 @@
+import { Gamepad2, Home, Search, User2 } from "lucide-react";
+import { NavLink } from "react-router-dom";
+
+const NavIcons = () => {
+
+  return (
+    <div className="flex items-center gap-3 max-sm:gap-1">
+      {/* HOME */}
+      <NavLink
+        to="/home"
+        className={({ isActive }) =>
+          `font-semibold flex items-center gap-2 max-sm:gap-1 px-3 py-2 rounded-md transition-colors ${
+            isActive
+              ? "bg-primary text-white hover:bg-primary/90"
+              : "hover:bg-accent"
+          }`
+        }
+      >
+        <Home size={20} />
+        <p className="max-md:hidden">Home</p>
+      </NavLink>
+      {/* GAMES */}
+      <NavLink
+        to="/games"
+        className={({ isActive }) =>
+          `font-semibold flex items-center gap-2 max-sm:gap-1 px-3 py-2 rounded-md transition-colors ${
+            isActive
+              ? "bg-primary text-white hover:bg-primary/90"
+              : "hover:bg-accent"
+          }`
+        }
+      >
+        <Gamepad2 size={20} />
+        <p className="max-md:hidden">Game</p>
+      </NavLink>
+      {/* SEARCH */}
+      <NavLink
+        to="/search"
+        className={({ isActive }) =>
+          `font-semibold flex items-center gap-2 max-sm:gap-1 px-3 py-2 rounded-md transition-colors ${
+            isActive
+              ? "bg-primary text-white hover:bg-primary/90"
+              : "hover:bg-accent"
+          }`
+        }
+      >
+        <Search size={20} />
+        <p className="max-md:hidden">Game</p>
+      </NavLink>
+      {/* PROFILE */}
+      <NavLink
+        to="/profile"
+        className={({ isActive }) =>
+          `font-semibold flex items-center gap-2 max-sm:gap-1 px-3 py-2 rounded-md transition-colors ${
+            isActive
+              ? "bg-primary text-white hover:bg-primary/90"
+              : "hover:bg-accent"
+          }`
+        }
+      >
+        <User2 size={20} />
+        <p className="max-md:hidden">Profile</p>
+      </NavLink>
+    </div>
+  );
+};
+
+export default NavIcons;
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,23 @@
+import { Gamepad2 } from "lucide-react"
+import NavIcons from "../NavIcons"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="bg-muted">
+      <nav className="h-16 bg-background border-b">
+        <div className="h-full flex items-center justify-between max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          {/* Logo */}
+          <div className="flex items-center gap-2 justify-center">
+            <Gamepad2 size={30} />
+            <h1 className="text-xl font-semibold">GamesCollect</h1>
+             {/* <ModeToggle /> */}
+          </div>
+
+          {/* Navigation Links */}
+          <NavIcons />
+        </div>
+      </nav>
+    </div>
   )
 }
 
