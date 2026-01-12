8c788b2f7119055adf8f7ac974606d35d9a1649a
thomas bartier <thomas.bartier@gmail.com>
2026-01-12 15:19:44 +0100

    add i18n

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,8 +3,10 @@ import CardGameUn from "@/components/cards/CardGameUn";
 import Header from "@/components/layout/Header";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import { useTranslation } from "react-i18next";
 
 const HomePage = () => {
+  const { t } = useTranslation();
   return (
     <div className="contenu">
       <Header
@@ -18,6 +20,7 @@ const HomePage = () => {
       </Header> */}
       <Button className={"p-4"}>Voir tout mes jeux</Button>
       <p>HomePage</p>
+      <p>LANGUE : {t("TEST")} </p>
       <br />
       <br />
       <br />
