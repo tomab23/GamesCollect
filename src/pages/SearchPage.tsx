99c72a09ebcfbfd80381d33f896e4c35ab08dfe0
thomas bartier <thomas.bartier@gmail.com>
2026-01-13 13:52:19 +0100

    add search input ui

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,12 +1,33 @@
-import Header from "@/components/layout/Header"
+import Header from "@/components/layout/Header";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { Search } from "lucide-react";
 
 const SearchPage = () => {
   return (
     <div className="contenu">
-      <Header children={undefined} isButton={false} page={""} nameButton={""} title={"Search"} />
-      <p>SearchPage</p>
-      </div>
-  )
-}
+      <Header
+        children={undefined}
+        isButton={false}
+        page={""}
+        nameButton={""}
+        title={"Search"}
+      />
 
-export default SearchPage
+      <div className="flex justify-center">
+        <InputGroup className="w-[70%]">
+          <InputGroupInput placeholder="Search..." />
+          <InputGroupAddon>
+            <Search />
+          </InputGroupAddon>
+          <InputGroupAddon align="inline-end">12 results</InputGroupAddon>
+        </InputGroup>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
