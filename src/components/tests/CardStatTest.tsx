66d8fd423ced5f2f672c31c8f0c6b9123c007f18
thomas bartier <thomas.bartier@gmail.com>
2026-01-13 13:48:35 +0100

    add card stat test

--- src/components/tests/CardStatTest.tsx
+++ src/components/tests/CardStatTest.tsx
@@ -1,11 +1,20 @@
-import { Card } from "../ui/card"
+import { Star } from "lucide-react";
+import { Card, CardContent } from "../ui/card";
 
 const CardStatTest = () => {
   return (
-    <Card>
-      stat
+    <Card className="w-60">
+      <CardContent className="flex items-center gap-3">
+        <div className="p-2 rounded-xl bg-primary/15">
+          <Star className="text-primary" />
+        </div>
+        <div>
+          <p className="text-xl font-semibold">20</p>
+          <p className="text-muted-foreground">Favorites</p>
+        </div>
+      </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default CardStatTest
+export default CardStatTest;
--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,7 +1,37 @@
+import CardGamesDeux from "@/components/tests/CardGamesDeux"
+import CardGameUn from "@/components/tests/CardGameUn"
+import CardStatTest from "@/components/tests/CardStatTest"
+import { GamesMockTest } from "@/helpers/GameMockTest"
 
 const TestPage = () => {
   return (
-    <div>TestPage</div>
+    <div className="contenu">
+      <p>CARD STAT TEST</p>
+      <div>
+        <CardStatTest />
+      </div>
+
+      <br /><br /><br />
+            <div>
+        <p>Test card - LIMITER LA LONGUEUR DUNE CARTE ET DU TITRE | GRID POUR CARD ??</p> <br />
+        <div>
+          <p>Test 1</p>
+          <div className="flex gap-4 flex-wrap">
+            {GamesMockTest.map((game) => (
+              <CardGameUn key={game.id} game={game} />
+            ))}
+          </div>
+          <br />
+          <br />
+          <p>Test 2</p>
+          <div className="flex gap-3 flex-wrap">
+            {GamesMockTest.map((game) => (
+              <CardGamesDeux key={game.id} game={game} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
