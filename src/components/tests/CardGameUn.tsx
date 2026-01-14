055303e64b3c8dd1b6184200559748d366d26745
thomas bartier <thomas.bartier@gmail.com>
2026-01-14 14:29:25 +0100

    update game cards test

--- src/components/tests/CardGameUn.tsx
+++ src/components/tests/CardGameUn.tsx
@@ -1,6 +1,13 @@
 import { Card, CardContent } from "@/components/ui/card";
 import type GameTest from "@/models/GameTest";
-import { Check, ChevronRight, Disc } from "lucide-react";
+import {
+  Check,
+  ChevronRight,
+  Computer,
+  Disc,
+  Gamepad2,
+  NotebookPen,
+} from "lucide-react";
 import { Link } from "react-router-dom";
 
 type Props = {
@@ -13,17 +20,34 @@ const CardGameUn = ({ game }: Props) => {
       <Card className="cursor-pointer hover:shadow-primary/20  max-md:w-full w-[35rem] hover:outline-1 ">
         <CardContent className="flex justify-between items-center">
           <div className="flex gap-4 items-center">
-            <h1 className="font-semibold truncate max-w-56" title={game.title}>
-              {game.title}
-            </h1>
+            <div className="flex gap-3 items-center">
+              <h1
+                className="font-semibold truncate max-w-48"
+                title={game.title}
+              >
+                {game.title}
+              </h1>
+              {game.pc ? (
+                <Computer className="h-4 w-4" />
+              ) : (
+                <Gamepad2 className="h-4 w-4" />
+              )}
+            </div>
             <p className="text-muted-foreground">{game.platform}</p>
-            <Check className="text-primary" />
-            <Disc className={`${!game.disc && "text-muted"}`} />
+            {/* ICONS */}
+            <div className="flex gap-2 items-center">
+              <Check
+                className={`${game.isFinish ? "text-primary" : "text-muted"}`}
+              />
+              <NotebookPen
+                size={21}
+                className={`${!game.disc && "text-muted"}`}
+              />
+              <Disc className={`${!game.disc && "text-muted"}`} />
+            </div>
           </div>
           <ChevronRight className="justify-self-end" />
         </CardContent>
-
-        <div className="absolute bottom-0 left-0 right-0 h-1 bg-linear-to-r from-primary to-accent transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
       </Card>
     </Link>
   );
