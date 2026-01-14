36cdb9f763fade5e442212a7f35c627089655ef5
thomas bartier <thomas.bartier@gmail.com>
2026-01-14 14:30:30 +0100

    delete card platform test 2

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,6 +1,5 @@
 import CardGamesDeux from "@/components/tests/CardGamesDeux";
 import CardGameUn from "@/components/tests/CardGameUn";
-import CardPlatformDeux from "@/components/tests/CardPlatformDeux";
 import CardStatPlatformTest from "@/components/tests/CardStatPlatformTest";
 import CardStatTest from "@/components/tests/CardStatTest";
 import { GamesMockTest } from "@/helpers/GameMockTest";
@@ -34,13 +33,6 @@ const TestPage = () => {
         <CardStatPlatformTest number={14} title={"Jeux sur Console"}>
           <Gamepad />
         </CardStatPlatformTest>
-        {/* TEST 2 */}
-        <CardPlatformDeux number={28} title={"Jeux sur Pc"}>
-          <Computer size={18} />
-        </CardPlatformDeux>
-        <CardPlatformDeux number={14} title={"Jeux sur Console"}>
-          <Gamepad size={18} />
-        </CardPlatformDeux>
       </div>
 
       <br />
